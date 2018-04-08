ba852e592db1a8c1205c6d40d13045c7d8478d93
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2018-04-08 20:17:00 +0700

    fix chat css

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
     var userbtn = $("#submitmsg")
     var chatbox = $("#chatbox")
 
+    usermsg.focus()
+
     var userid = getRandId()
     var users = []
     var defaultName = "user_" + userid.toString()
@@ -13,17 +15,18 @@ $(document).ready(function () {
     $("#yourName").html(defaultName)
 
 
-    $("#exit").click(function() {
+
+    $("#exit").click(function () {
         if (confirm('Are you sure want to exit this conversation?')) {
             socket.emit("end")
             window.location = "/"
         } else {
             // Do nothing
         }
-        
+
     })
 
-    $("#yourName").click(function(e) {
+    $("#yourName").click(function (e) {
         e.preventDefault()
         changeName(null)
     })
@@ -44,7 +47,7 @@ $(document).ready(function () {
         if ($.trim(message) == '') {
             alert('Input can not be left blank');
         } else {
-            if(message[0] == '/') {
+            if (message[0] == '/') {
                 processQuery(message)
 
                 usermsg.val("")
@@ -69,29 +72,29 @@ $(document).ready(function () {
     socket.on('receiv', function (receiv) {
         displayChat(receiv.name, receiv.message)
     })
-    
-    socket.on('left', function(data) {
+
+    socket.on('left', function (data) {
         var header = "<b>" + data.name + "</b> "
         var content = "<span class='info'>" + header + "has <span class='error'>left</span>" + "</span>"
         displayToLog(content)
     })
 
-    socket.on('new', function(data) {
+    socket.on('new', function (data) {
         var header = "<b>" + data.name + "</b> "
         var content = "<span class='info'>" + header + " has <span class='success'>joined</span>" + "</span>"
         displayToLog(content)
     })
 
-    socket.on('usernewname', function(data) {
+    socket.on('usernewname', function (data) {
         var header = "An user with ID <b>" + data.user + "</b> has changed name to "
-        var content = "<span class='info'>" +header + "<b>" + data.name + "</b></span>"
+        var content = "<span class='info'>" + header + "<b>" + data.name + "</b></span>"
         displayToLog(content)
     })
 
-    socket.on('stats_count', function(data) {
+    socket.on('stats_count', function (data) {
         var m = "<span class='info'>"
         m += "Online: " + data.total + "<br>"
-        data.clients.forEach(function(item) {
+        data.clients.forEach(function (item) {
             m += "<span class='success'>" + item.name + "<span><br>"
         })
         m += "</span>"
@@ -110,15 +113,15 @@ $(document).ready(function () {
 
     function displayToLog(content) {
         chatbox.append(content)
-        .append("<br>")
-        .animate({
-            scrollTop: chatbox
-                .prop("scrollHeight")
-        }, 500);
+            .append("<br>")
+            .animate({
+                scrollTop: chatbox
+                    .prop("scrollHeight")
+            }, 500);
     }
 
     function processQuery(cmd) {
-        if(cmd[1] === '?' || cmd[1] === "" || cmd[1] === " ") {
+        if (cmd[1] === '?' || cmd[1] === "" || cmd[1] === " ") {
             var help = "<span class='info'>--------------------------------------------------------------------<br>"
             help += "Help:<br>"
             help += "--------------------------------------------------------------------<br>"
@@ -128,41 +131,44 @@ $(document).ready(function () {
             help += "- type <b>/online</b> to show all online users"
             help += "- </span>"
             chatbox.append(help)
-            .append("<br>")
-            .animate({
-                scrollTop: chatbox
-                    .prop("scrollHeight")
-            }, 500);
-        } else if(cmd === "/id") {
+                .append("<br>")
+                .animate({
+                    scrollTop: chatbox
+                        .prop("scrollHeight")
+                }, 500);
+        } else if (cmd === "/id") {
             var m = "<span class='info'>Your ID: <span class='success'>" + userid + "</span></span>";
             chatbox.append(m)
-            .append("<br>")
-            .animate({
-                scrollTop: chatbox
-                    .prop("scrollHeight")
-            }, 500);
-        } else if(cmd.startsWith("/name ")) {
+                .append("<br>")
+                .animate({
+                    scrollTop: chatbox
+                        .prop("scrollHeight")
+                }, 500);
+        } else if (cmd.startsWith("/name ")) {
             var arr = cmd.split(' ')
             var newname = arr[1]
-            if(newname && newname != "" && newname != " ") changeName(newname)
-        } else if(cmd.startsWith('/online')) {
+            if (newname && newname != "" && newname != " ") changeName(newname)
+        } else if (cmd.startsWith('/online')) {
             socket.emit('stats')
         }
     }
 
     function changeName(name) {
-        if(name) {
+        if (name) {
             newName = name
         } else {
+            // bootbox.prompt("What's your name?", function (result) {
+            //     newName = result.trim()
+            // });
             newName = prompt("What's your name?", defaultName).trim()
         }
-        if(newName != null || newName != "" || newName != defaultName) {
-            $("#yourName").html(newName) 
+        if (newName != null || newName != "" || newName != defaultName) {
+            $("#yourName").html(newName)
             socket.emit("changename", {
                 user: userid,
                 name: newName
             })
         }
-        
+
     }
 })
